56b379bae2c8f90438d9eafa49c390dfa9afd9b1
Mollie Stein <mollie.stein@gmail.com>
2017-05-29 13:09:20 -0400

    added array for MES preference

--- lib/personalized_recipes/CLI.rb
+++ lib/personalized_recipes/CLI.rb
@@ -2,7 +2,7 @@ class PersonalizedRecipes::CLI
 
   attr_accessor :recipes, :total, :ingredients_to_remove, :recipes_all
 
-  #@@ingredients_to_remove = ["instant", "chicken", "beef", "steak", "bacon", "pork", "ham", "cilantro", "orange", "oranges"]
+  @@ingredients_to_remove_mes = ["instant", "chicken", "beef", "steak", "bacon", "pork", "ham", "cilantro", "orange", "oranges"]
 
   def initialize
     @@ingredients_to_remove = []
@@ -17,7 +17,6 @@ class PersonalizedRecipes::CLI
     PersonalizedRecipes::Recipe.all
   end
 
-
   def start
     welcome_message
     call_scrape
@@ -26,30 +25,33 @@ class PersonalizedRecipes::CLI
     end_program
   end
 
+  def welcome_message
+    puts "\n------- Welcome to Personalized Recipes -------"
+    remove_ingredients
+    puts "\nPulling featured recipes from Food52 without the following ingredients...\n\n"
+    @@ingredients_to_remove.each{ |item|
+      puts item
+    }
+  end
+
   def remove_ingredients
-    puts "\nWhich ingredients would you like to exclude?  \nInput one word at a time without spaces.  \nInput none for all recipes.  \nInput done when finished.  \n"
+    puts "\nWhich ingredients would you like to exclude?  \n  Input one word at a time without spaces  \n  Input none for all recipes  \n  Input done when finished  \n"
     input = nil
     while input != "done"
       input = gets.strip.downcase
       if input.to_i > 0
         puts "Invalid input."
-      elsif input = "none"
+      elsif input == "none"
         @@ingredients_to_remove = Array.new
+        input = "done"
+      elsif input == "mes"
+        @@ingredients_to_remove = @@ingredients_to_remove_mes
         input = "done"
       elsif input == "done"
       else
         @@ingredients_to_remove << input
       end
     end
-  end
-
-  def welcome_message
-    puts "\n------- Welcome to Personalized Recipes -------"
-    remove_ingredients
-    puts "\nPulling recipes from Food52 without the following ingredients...\n\n"
-    @@ingredients_to_remove.each{ |item|
-      puts item
-    }
   end
 
   def call_scrape
@@ -66,7 +68,7 @@ class PersonalizedRecipes::CLI
     @@total = gets.strip.to_i - 1
     if @@total < 0 || @@total > recipes_all.length - 1
       puts "Invalud input."
-      start
+      list_recipes
     end
     puts "\n-------  Recipes  -------\n\n"
     @recipes = recipes_all[0..@@total]
@@ -98,7 +100,7 @@ class PersonalizedRecipes::CLI
   end
 
   def end_program
-    puts "Bon appetit!"
+    puts "Bon Appetit!"
   end
 
 end
